1eb4ca022c3a041f810500fef5b4c9acd6b8d143
Pablo <pablolocha1@gmail.com>
2023-04-17 22:25:38 -0400

    Calcular

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 class Factorial{
 public: 
-int n;
+int n, resultado;
 int Calcular(){
-    
+	int numero1 = 1;
+	int numero2 = 1;
+	for (int i = 0; i < n; i++)
+	{
+		resultado = numero1 * numero2;
+		numero2 = numero2 + 1;
+		numero1 = resultado;
+	}
+	cout << "el factorial es:" << resultado << endl;
 }
 };
 using namespace std;
